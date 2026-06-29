3281cfb132048831ccf076f05a7b45f79c450104
Anit Paul <anit42655@gmail.com>
2026-06-29 14:32:17 +0000

    pk send by url and handled by serializers

--- drf/api/views.py
+++ drf/api/views.py
@@ -2,9 +2,27 @@ from myapp.models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from django.shortcuts import render, get_object_or_404
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentAPI(request):
-    students = Student.objects.all()
-    serializer = StudentSerializer(students, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer=StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET'])     
+def studentIDAPI(request,pk):
+    if request.method == 'GET':
+        student = get_object_or_404(Student, pk=pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
